ae0dd7e06ad2a3f258b468753fd8e41e039660ac
sfghsslkgfshg <bluezenithlol@gmail.com>
2025-10-03 16:55:50 +0800

    fix cogs/help.py 3

--- cogs/help.py
+++ cogs/help.py
@@ -8,10 +8,10 @@ class Help(commands.Cog):
     @commands.command()
     async def help(self, ctx):
         e = discord.Embed(title="Help", color=discord.Colour.yellow())
-        e.add_field(name="setup", value="Setup a dynamic voice channel system (you can setup multiple ones).")
-        e.add_field(name="limit", value="Set the limit of the voice channel. Not entering a value makes it a one-person only vc.")
-        e.add_field(name="perm", value="Set the permission of the voice channel. You can enter multiple user names to add them once.")
-        e.add_field(name="name", value="Set the voice channel's name.")
+        e.add_field(name="setup", value="Setup a dynamic voice channel system (you can setup multiple ones).", inline=False)
+        e.add_field(name="limit", value="Set the limit of the voice channel. Not entering a value makes it a one-person only vc.", inline=False)
+        e.add_field(name="perm", value="Set the permission of the voice channel. You can enter multiple user names to add them once.", inline=False)
+        e.add_field(name="name", value="Set the voice channel's name.", inline=False)
         await ctx.send(embed=e)
 
 async def setup(bot):
